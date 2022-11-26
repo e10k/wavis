e118d4e70a5452e7958b6d642c1b570590bbe517
e10k <e@e10k.dev>
2022-11-26 12:19:34 +0200

    udpated tests

--- main_test.go
+++ main_test.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"testing"
+	"wav/parser"
+	"wav/utils"
 )
 
 func TestToMonoSamples(t *testing.T) {
-	w := &Wav{
+	w := &parser.Wav{
 		Data: [][]int32{
 			{0, 0, 5, 5, 12345, 12345, -12345},
 			{0, 1, 7, 8, 67890, -12345, 0},
 		},
 	}
 
-	given := w.toMonoSamples()
+	given := w.GetMonoSamples()
 	expected := []int32{0, 0, 6, 6, 40117, 0, -6172}
 
 	var i int
@@ -24,7 +26,7 @@ func TestToMonoSamples(t *testing.T) {
 }
 
 func TestScaleBetween(t *testing.T) {
-	given := scaleBetween([]int32{-4, 0, 5, 6, 9}, 0, 100)
+	given := utils.ScaleBetween([]int32{-4, 0, 5, 6, 9}, 0, 100)
 	expected := []int32{0, 30, 69, 76, 100}
 
 	for i, _ := range expected {
